6d7394a9af5d7a0377074a920aaa2378fbb6ae46
Hannah McFarlane <hannah.mcfarlane@bt.com>
2023-09-25 17:42:25 +0100

    display count

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import "./App.css";
 import WebFont from "webfontloader";
@@ -15,12 +15,15 @@ function App() {
     WebFont.load(fonts);
   });
 
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Rat Race 🐀</h1>
       </header>
       <main>
+        <p>{count}</p>
         <h2>Welcome to the Afterlife Casino!</h2>
         <p>Please gamble responsibly.</p>
         <RatRace />
